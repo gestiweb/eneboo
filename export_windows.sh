c64177e44d5ba6f0ac023da0a962f4da1515c3c5
David Martinez Marti <desarrollo@gestiweb.com>
2011-09-30 10:10:40 +0200

    Dependencias y exportación para Windows

---
+++ export_windows.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+SRC=src/qt
+DST=abanq-build-win32
+
+mkdir "$DST" 2>/dev/null
+mkdir "$DST/bin" 2>/dev/null
+mkdir "$DST/lib" 2>/dev/null
+mkdir "$DST/plugins" 2>/dev/null
+mkdir "$DST/plugins/designer" 2>/dev/null
+mkdir "$DST/plugins/styles" 2>/dev/null
+mkdir "$DST/plugins/sqldrivers" 2>/dev/null
+
+cp "$SRC/bin/"*.exe "$DST/bin"
+cp "$SRC/bin/"*.dll "$DST/bin"
+cp "$SRC/lib/"*.dll "$DST/lib"
+cp "$SRC/plugins/designer/"*.dll "$DST/plugins/designer" 
+cp "$SRC/plugins/styles/"*.dll "$DST/plugins/styles" 
+cp "$SRC/plugins/sqldrivers/"*.dll "$DST/plugins/sqldrivers" 
+cp -R "$SRC/share" "$DST/" 
+
+
+
+
