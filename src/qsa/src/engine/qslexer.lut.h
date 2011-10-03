42e37b9234875fba8b1ba742170f60a7872c4e73
David Martinez Marti <desarrollo@gestiweb.com>
2011-10-03 13:07:21 +0200

    Corrección error infoSial en generación del lexer

--- src/qsa/src/engine/qslexer.lut.h
+++ src/qsa/src/engine/qslexer.lut.h
@@ -1,31 +1,3 @@
-/****************************************************************************
-** $Id: qslexer.lut.h  1.1.5   edited 2006-02-23T15:39:57$
-**
-** Copyright (C) 2001-2006 Trolltech AS.  All rights reserved.
-**
-** This file is part of the Qt Script for Applications framework (QSA).
-**
-** This file may be distributed and/or modified under the terms of the
-** GNU General Public License version 2 as published by the Free Software
-** Foundation and appearing in the file LICENSE.GPL included in the
-** packaging of this file.
-**
-** Licensees holding a valid Qt Script for Applications license may use
-** this file in accordance with the Qt Script for Applications License
-** Agreement provided with the Software.
-**
-** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-**
-** See http://www.trolltech.com/pricing.html or email sales@trolltech.com for
-**   information about QSA Commercial License Agreements.
-** See http://www.trolltech.com/gpl/ for GPL licensing information.
-**
-** Contact info@trolltech.com if any conditions of this licensing are
-** not clear to you.
-**
-*****************************************************************************/
-
 /* automatically generated from keywords.table. DO NOT EDIT ! */
 
 const struct QSHashEntry mainTableEntries[] = {
@@ -105,4 +77,4 @@ const struct QSHashEntry mainTableEntries[] = {
    { "volatile", RESERVED, 0, 0 }
 };
 
-const struct QSHashTable mainTable = { 2, 74, mainTableEntries, 40 };
+const struct QSHashTable mainTable = { 2, 74, mainTableEntries, 41 };
---
+++ src/qsa/src/engine/update_hash_table.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./create_hash_table keywords.table > qslexer.lut.h
