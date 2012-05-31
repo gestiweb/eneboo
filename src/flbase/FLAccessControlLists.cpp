a93d1743fc7c7362b91776a085114179555cd7e2
José A. Fernández <aullasistemas@gmail.com>
2012-05-31 02:15:41 +0200

    Se añade debug para ACL

--- src/flbase/FLAccessControlLists.cpp
+++ src/flbase/FLAccessControlLists.cpp
@@ -86,6 +86,8 @@ void FLAccessControlLists::init(const QString &aclXml)
         accessControlList_->replace(ac->type() + QString::fromLatin1("::") +
                                     ac->name() + QString::fromLatin1("::") +
                                     ac->user(), ac);
+	qWarning("FLAccessControlLists::set: " +
+           ac->type() + " :: " + ac->name() + " :: " + ac->user() );
         no = no.nextSibling();
         continue;
       }
