2e8deef1f26f920d47439627da3c678b78a2c929
José A. Fernández <aullasistemas@gmail.com>
2012-10-08 01:19:36 +0200

    Algunos cambios mac cross

--- build_mac_cross_ppc.sh
+++ build_mac_cross_ppc.sh
@@ -2,4 +2,4 @@
 export CROSS='powerpc-apple-darwin8-'
 export PATH=$PATH:/opt/mac/bin
 
-./build.sh -platform "macx-g++-cross" -quick
+./build.sh -platform "macx-g++-cross" -quick && ./export_mac_cross.sh
