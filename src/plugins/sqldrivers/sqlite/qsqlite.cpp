5eb84321207710c32bd3d4a5d6ff1a0d76bf9692
Adrián Bernardi <adrian.bernardi@gmail.com>
2012-10-17 10:17:38 -0300

    Revert "Mr Propper: borrar las imágenes no utilizadas de artículos."
    This reverts commit 497dd3af674fc8e49bb1b2fa4031c1582db3137e.

--- src/plugins/sqldrivers/sqlite/qsqlite.cpp
+++ src/plugins/sqldrivers/sqlite/qsqlite.cpp
@@ -1084,19 +1084,4 @@ void SqliteDriver::Mr_Proper()
   qWarning("vacuum");
 #endif
   FLUtil::destroyProgressDialog();
-
-  // Silix
-  steps = 0;
-  FLUtil::createProgressDialog(tr("Borrando imágenes obsoletas"), 1);
-
-  if (db_->manager()->existsTable("fllarge_articulos")) {
-#ifdef FL_DEBUG
-    qWarning("Borrando imágenes obsoletas en fllarge_articulos");
-#endif
-    qry.exec("delete from fllarge_articulos where refkey not in (select imagen from articulos where imagen is not null)");
-  }
-  FLUtil::setProgress(++steps);
-
-  FLUtil::destroyProgressDialog();
-  //
 }
