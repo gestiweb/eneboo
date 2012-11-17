28a65174fbb1031eca74ef654b9a281361e6b37d
Adrián Bernardi <adrian.bernardi@gmail.com>
2012-11-17 10:22:30 -0300

    FLFieldDB: poder seleccionar el texto en los campos deshabilitados (2)

--- src/flbase/FLFieldDB.cpp
+++ src/flbase/FLFieldDB.cpp
@@ -2487,6 +2487,7 @@ void FLFieldDB::setDisabled(const bool b)
     case QVariant::Time:
       if (editor_ && ::qt_cast<QTimeEdit *>(editor_)) {
         ::qt_cast<QTimeEdit *>(editor_)->setDisabled(b);
+        ::qt_cast<QLabel *>(textLabelDB)->setDisabled(b);
         ::qt_cast<QTimeEdit *>(editor_)->setPaletteBackgroundColor(cFondo);
       }
       break;
